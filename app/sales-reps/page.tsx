5dd20360be44948430674fb710eb57db947509fc
Ahmed ALi <36568764+ahmedali20@users.noreply.github.com>
2026-07-23 21:15:51 +0300

    Update page.tsx

--- app/sales-reps/page.tsx
+++ app/sales-reps/page.tsx
@@ -12,7 +12,10 @@ export const revalidate = 0;
 export default async function SalesRepsPage() {
   const { data, error } = await supabaseServer
     .from("sales_view")
-    .select("id, sales_rep, customer_name, total_sales");
+    .select(
+      "id, invoice_no, sales_date, month, sales_rep, customer_name, sales_item_total, tax, total_sales"
+    )
+    .order("sales_date", { ascending: false });
 
   if (error) {
     return <main style={{ padding: 32, color: "red" }}>{error.message}</main>;
